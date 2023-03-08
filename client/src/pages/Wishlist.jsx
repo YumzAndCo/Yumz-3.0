ff881ae67bc38a8293542c2ea8f2a8f12748bcd1
Daniel Chang <dkc213@Daniels-MacBook-Pro.local>
2023-03-08 11:34:18 -0800

    frontend ready for connection to backend Co-authored by: Cole Bryan <CNBryan129@gmail.com>

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -3,18 +3,33 @@ import { CollectionList } from './CollectionList.jsx';
 
 export const Wishlist = () => {
 
-  const [listItems, setListItems] = useState({});
+  const [listItems, setListItems] = useState([{
+    name: 'Ramen House',
+    rating: 8,
+    cuisine: 'Japanese',
+    hours: '11 am - 8 pm, 7 days/wk',
+    preview: 'Lorem ipsum...',
+    id: 1
+  },
+  {
+    name: 'Ramen place',
+    rating: 8,
+    cuisine: 'Japanese',
+    hours: '11 am - 8 pm, 7 days/wk',
+    preview: 'Lorem ipsum...',
+    id: 2
+  }]);
 
-  useEffect(() => {
-    async function fetchReviews() {
-      return fetch('/wishlist')
-        .then(data => data.json())
-        .then(data => setListItems(data));
-    }
-    fetchReviews();
-  });
+  // useEffect(() => {
+  //   async function fetchReviews() {
+  //     return fetch('/wishlist')
+  //       .then(data => data.json())
+  //       .then(data => setListItems(data));
+  //   }
+  //   fetchReviews();
+  // });
   return (
-    <CollectionList listName="Wishlist"/>
+    <CollectionList restaurants = {listItems} listName="Wishlist"/>
   );
 };
 
--- server/src/controllers/restaurantController.js
+++ server/src/controllers/restaurantController.js
@@ -5,8 +5,9 @@ const restaurantController = {};
 
 restaurantController.addRestaurant = async (req, res, next) => {
   try{
-    if(req.body.restaurant.restaurant_id !== null) {
-      res.locals.restID = req.body.restaurant_id;
+    const queryRestaurant = await db.query(`SELECT * FROM restaurants WHERE name = '${name}' AND address = '${address}'`);
+    if(queryRestaurant.rowCount !== 0) {
+      res.locals.restID = queryRestaurant.rows[0].restaurant_id;
       return next();
     }
     const {name, cuisine, price_rating, hours, address, delivery, menu_url} = req.body.restaurant;
@@ -14,7 +15,7 @@ restaurantController.addRestaurant = async (req, res, next) => {
       `INSERT INTO restaurants (name, cuisine, price_rating, hours, address, delivery, menu_url) 
       VALUES ('${name}', '${cuisine}', '${price_rating}', '${hours}', '${address}', '${delivery}', '${menu_url}')`
     );
-    const newRestaurant = await db.query(`SELECT * FROM restaurants WHERE name = '${name}' AND address = '${address}'`)
+    const newRestaurant = await db.query(`SELECT * FROM restaurants WHERE name = '${name}' AND address = '${address}'`);
     res.locals.restID = newRestaurant.rows[0].restaurant_id;
     return next();
 
